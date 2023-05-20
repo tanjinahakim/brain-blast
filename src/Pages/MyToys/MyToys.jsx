d3bdb3eac1625fae8468e278cb0e3ce13f01bc06
tanjinahakim <tanjinahakim44@gmail.com>
2023-05-20 17:38:39 +0600

    delete data

--- src/Pages/MyToys/MyToys.jsx
+++ src/Pages/MyToys/MyToys.jsx
@@ -2,7 +2,9 @@ import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../../providers/AuthProvider";
 import { FaEye, FaPencilAlt, FaTrash } from "react-icons/fa";
 import { Link } from "react-router-dom";
+import Swal from "sweetalert2";
 const MyToys = () => {
+    let i=0;
     const {user} = useContext(AuthContext);
     const [myToys,setMyToys]=useState([]);
     const url = `http://localhost:5000/allToys?email=${user?.email}`;
@@ -11,7 +13,37 @@ const MyToys = () => {
         .then(res=>res.json())
         .then(data=>setMyToys(data))
     }, [])
-    let i=0;
+    const handleDelete = id =>{
+        Swal.fire({
+            title: 'Are you sure?',
+            text: "You won't be able to revert this!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, delete it!'
+          }).then((result) => {
+            if (result.isConfirmed) {
+           
+            fetch(`http://localhost:5000/allToys/${id}`,{
+                method:'DELETE'
+            })
+            .then(res=>res.json())
+            .then(data=>{
+                console.log(data);
+                if(data.deletedCount>0){
+                       Swal.fire(
+                'Deleted!',
+                'Your product has been deleted.',
+                'success'
+              )
+              const remaining = myToys.filter(toy=>toy._id !== id)
+              setMyToys(remaining);
+                }
+            })
+            }
+          })
+    }
     return (
         <>
            <div className="lg:overflow-x-auto overflow-hidden">
@@ -46,7 +78,7 @@ const MyToys = () => {
                                 <div className="lg:space-x-2 space-y-2">
                                 <Link to={`/toy/${toy._id}`}><button className="btn btn-outline btn-primary btn-sm"><FaEye></FaEye></button></Link>
                                 <button className="btn btn-outline btn-accent btn-sm"><FaPencilAlt></FaPencilAlt></button>
-                                <button className="btn btn-outline btn-error btn-sm"><FaTrash></FaTrash></button>
+                                <button onClick={()=>handleDelete(toy._id)} className="btn btn-outline btn-error btn-sm"><FaTrash></FaTrash></button>
                                 </div>
                             </td>
                             </tr>
